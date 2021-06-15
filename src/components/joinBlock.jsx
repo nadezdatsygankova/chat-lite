27a4ae887dcdeacec20d21809a2d35ace1d2bcc8
Nadezda <nadia_pritchina@gmail.com>
2021-06-15 12:17:00 -0400

    finish version

--- src/components/joinBlock.jsx
+++ src/components/joinBlock.jsx
@@ -1,16 +1,47 @@
 
+import { useState } from 'react';
 import socket from './../socket';
+import React from 'react';
+import axios from 'axios';
 
+function JoinBlock({ onLogin }) {
+  const [roomId, setRoomId] = React.useState('');
+  const [userName, setUserName] = React.useState('');
+  const [isLoading, setLoading] = React.useState(false);
 
-function JoinBlock() {
-    return(
-        <div className="join-block">
-       <input type='text' placeholder="Room ID" />
-       <input type='text' placeholder="Your name" />
-       <button className="btn btn-success">Enter</button>
-        </div>
+  const onEnter = async () => {
+    if (!roomId || !userName) {
+      return alert('Invalid input');
+    }
+    const obj = {
+      roomId,
+      userName,
+    };
+    setLoading(true);
+    await axios.post('/rooms', obj);
+    onLogin(obj);
+  };
 
-    )
+  return (
+    <div className="join-block">
+      <input
+        type="text"
+        placeholder="Room ID"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Enter name"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button disabled={isLoading} onClick={onEnter} className="btn btn-success">
+        {isLoading ? 'Enter...' : 'Exit'}
+      </button>
+    </div>
+  );
 }
 
-export default JoinBlock
+export default JoinBlock;
+
